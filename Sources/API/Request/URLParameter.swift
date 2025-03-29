b202ba3fa36bdca369ac3752d88d85c0c138eb63
Arthur Zavolovych <boredarthur@gmail.com>
2025-03-29 12:54:58 +0200

    custom url parameters

--- Sources/API/Request/URLParameter.swift
+++ Sources/API/Request/URLParameter.swift
@@ -8,14 +8,14 @@
 import Foundation
 
 /// Standard URL parameters for API requests.
-public enum URLParameter: String {
+public enum URLParameter: Hashable {
     // Pagination parameters
     case page
-    case perPage = "per_page"
+    case perPage
     
     // Sorting and filtering
-    case sortBy = "sort_by"
-    case sortOrder = "sort_order"
+    case sortBy
+    case sortOrder
     case filter
     case query
     case search
@@ -24,32 +24,85 @@ public enum URLParameter: String {
     case email
     case password
     case token
-    case refreshToken = "refresh_token"
-    case apiKey = "api_key"
-    case currentDatetime = "current_datetime"
+    case refreshToken
+    case apiKey
+    case currentDatetime
     
     // User-related parameters
-    case firstName = "first_name"
-    case lastName = "last_name"
-    case userName = "user_name"
-    case displayName = "display_name"
-    case profileImage = "profile_image"
-    case dateOfBirth = "date_of_birth"
+    case firstName
+    case lastName
+    case userName
+    case displayName
+    case profileImage
+    case dateOfBirth
     case gender
-    case phoneNumber = "phone_number"
+    case phoneNumber
     
     // Location parameters
-    case latitude = "lat"
-    case longitude = "lon"
+    case latitude
+    case longitude
     case radius
     
     // Miscellaneous
     case language
     case version
     case platform
-    case deviceId = "device_id"
+    case deviceId
     case timezone
     case format
+    
+    // Custom parameters
+    case custom(String)
+    
+    /// The raw string value of the parameter.
+    public var rawValue: String {
+        switch self {
+        // Pagination parameters
+        case .page: return "page"
+        case .perPage: return "per_page"
+            
+        // Sorting and filtering
+        case .sortBy: return "sort_by"
+        case .sortOrder: return "sort_order"
+        case .filter: return "filter"
+        case .query: return "query"
+        case .search: return "search"
+            
+        // Authentication and session
+        case .email: return "email"
+        case .password: return "password"
+        case .token: return "token"
+        case .refreshToken: return "refresh_token"
+        case .apiKey: return "api_key"
+        case .currentDatetime: return "current_datetime"
+            
+        // User-related parameters
+        case .firstName: return "first_name"
+        case .lastName: return "last_name"
+        case .userName: return "user_name"
+        case .displayName: return "display_name"
+        case .profileImage: return "profile_image"
+        case .dateOfBirth: return "date_of_birth"
+        case .gender: return "gender"
+        case .phoneNumber: return "phone_number"
+            
+        // Location parameters
+        case .latitude: return "lat"
+        case .longitude: return "lon"
+        case .radius: return "radius"
+            
+        // Miscellaneous
+        case .language: return "language"
+        case .version: return "version"
+        case .platform: return "platform"
+        case .deviceId: return "device_id"
+        case .timezone: return "timezone"
+        case .format: return "format"
+            
+        // Custom parameters
+        case .custom(let param): return param
+        }
+    }
 }
 
 // Extension for creating URL query items
